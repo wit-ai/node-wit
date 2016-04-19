fd662aaad0521b1710b67b97b381277e927482cc
Julien Odent <patapizza@gmail.com>
2016-04-19 15:15:53 -0700

    fixing ngrok http param in example

--- examples/messenger.js
+++ examples/messenger.js
@@ -8,7 +8,7 @@
 //
 // 1. npm install body-parser express request 
 // 2. Download and install ngrok from https://ngrok.com/download
-// 3. ./ngrok -http 8445
+// 3. ./ngrok http 8445
 // 4. WIT_TOKEN=your_access_token FB_PAGE_ID=your_page_id FB_PAGE_TOKEN=your_page_token FB_VERIFY_TOKEN=verify_token node examples/messenger.js
 // 5. Subscribe your page to the Webhooks using verify_token and `https://<your_ngrok_io>/fb` as callback URL.
 // 6. Talk to your bot on Messenger!
